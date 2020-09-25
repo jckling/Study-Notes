a55f4548bf680ce6e26aaa1985ba4077cb8a3a6e
jckling <jckling@163.com>
2020-09-25 22:30:04 +0800

    Study Python Tutorial 10-11

---
+++ The Python Tutorial/top.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser(prog = 'top',
+    description = 'Show top lines from each file')
+parser.add_argument('filenames', nargs='+')
+parser.add_argument('-l', '--lines', type=int, default=1)
+args = parser.parse_args()
+print(args)
