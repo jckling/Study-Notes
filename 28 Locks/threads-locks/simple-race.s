de45bab67e0aa364eb3d73c708a6f7a35b597547
jckling <jckling@163.com>
2021-01-14 16:29:16 +0800

    delete redundant files

--- 28 Locks/threads-locks/simple-race.s
+++
@@ -1,5 +0,0 @@
-.main
-mov 2000, %ax   # get the value at the address
-add $1, %ax     # increment it
-mov %ax, 2000   # store it back
-halt
--- 37 Hard Disk Drives/file-disks/draw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {1..1000};
-do
-    tmp=($(./disk.py -a -1 -A 1000,-1,0 -w ${i} -p SATF -c | grep TOTALS | grep -Eo '[0-9]+'))
-    $(printf "%s " ${i} >> data)
-    $(printf "%s " "${tmp[@]}" >> data)
-    $(echo >> data)
-done
